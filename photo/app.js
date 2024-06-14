2b359b00b1b85b04c73fbebc9fea1d768853d0d8
ehdgusdl9177 <k23129177@gmail.com>
2024-06-14 19:54:16 +0900

    photo finish

--- photo/app.js
+++ photo/app.js
@@ -26,15 +26,36 @@ fs.promises.readdir(workingDir).then(processFiles).catch(console.log);
 function processFiles(files) {
   files.forEach((file) => {
     if (isVideoFile(file)) {
-      console.log("video", file);
+      move(file, videoDir);
     } else if (isCapturedFile(file)) {
-      console.log("captured", file);
-    } else if (isDuplicatedFile(file)) {
-      console.log("duplicated", file);
+      move(file, capturedDir);
+    } else if (isDuplicatedFile(files, file)) {
+      move(file, duplicatedDir);
     }
   });
 }
 
-function isVideoFile(file) {}
-function isCapturedFile(file) {}
-function isDuplicatedFile(file) {}
+function isVideoFile(file) {
+  const regExp = /(mp4|mov)$/gm;
+  const match = file.match(regExp);
+  return !!match;
+}
+function isCapturedFile(file) {
+  const regExp = /(png|aae)$/gm;
+  const match = file.match(regExp);
+  return !!match;
+}
+function isDuplicatedFile(files, file) {
+  if (!file.startsWith("IMG_") || file.startsWith("IMG_E")) {
+    return false;
+  }
+  const edited = `IMG_E${file.split("_")[1]}`;
+  const found = files.find((f) => f.includes(edited));
+  return !!found;
+}
+
+function move(file, targetDir) {
+  const oldPath = path.join(workingDir, file);
+  const newPath = path.join(targetDir, file);
+  fs.promises.rename(oldPath, newPath).catch(console.error);
+}
