0df95cb820d95c583e9ea2fda572a16a307d08dc
ehdgusdl9177 <k23129177@gmail.com>
2024-06-21 19:11:34 +0900

    express middleware

--- cors/app.js
+++ cors/app.js
@@ -1,8 +1,14 @@
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
+import helmet from "helmet";
 
 const app = express();
 
+app.use(express.json());
+app.use(cookieParser());
+app.use(morgan("combined"));
 app.use(
   cors({
     origin: ["http://127.0.0.1:5000"],
@@ -10,8 +16,11 @@ app.use(
     credentials: true, // Access-Control-Allow-Credntials: true
   })
 );
+app.use(helmet());
 
 app.get("/", (req, res) => {
+  console.log(req.body);
+  console.log(req.cookies);
   res.send("Welcome");
 });
 
